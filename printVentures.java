5d0fff7f96cd743d75328b189b2d428a35aae530
Ezebitz <121066012+Ezebitz@users.noreply.github.com>
2023-04-14 06:01:10 -0700

    Add files via upload

---
+++ printVentures.java
@@ -0,0 +1,10 @@
+package problemSolving;
+
+public class printVentures {
+    public static void main(String[] args) {
+        string numbers = "ventures";
+        for( i=0;i<number.length; i+=2){
+            System.out.println("numbers");
+        }
+    }
+}
---
+++ sumOfIntegers.java
@@ -0,0 +1,15 @@
+package problemSolving;
+
+public class sumOfIntegers {
+    public static void main(String[] args) {
+        int sum = 0;
+        int counter = 1;
+        while (counter < 30) {
+            if (counter % 3 == 0) {
+                sum += counter;
+
+            }
+            System.out.println("sum");
+        }
+    }
+}  
